77b0689cba447c465b2285148e85a42aef5a5ff0
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2020-02-03 18:12:40 +0100

    Add list of key features

--- src/build.js
+++ src/build.js
@@ -1,6 +1,8 @@
 const Metalsmith  = require("metalsmith");
 const argv = require("yargs").argv;
 const fs = require("fs");
+const hyphenopoly = require("hyphenopoly");
+const nunjucks = require("nunjucks");
 
 const htmlMinifier = require("metalsmith-html-minifier");
 const layouts = require("metalsmith-layouts");
@@ -10,6 +12,19 @@ const rename = require("metalsmith-rename");
 const sass = require("metalsmith-sass");
 const uglify = require("metalsmith-uglify");
 const watch = require("metalsmith-watch");
+
+const hyphenateText = hyphenopoly.config({
+  "require": ["en-gb"],
+  "hyphen": "\u00ad",
+  "exceptions": {
+    "en-gb": "plug-in"
+  },
+  "sync": true
+});
+
+function hyphenFilter(str) {
+    return new nunjucks.runtime.SafeString(hyphenateText(str));
+}
 
 fs.mkdirSync("js", { recursive: true });
 fs.copyFileSync("node_modules/headroom.js/dist/headroom.min.js", "js/headroom.min.js");
@@ -26,7 +41,13 @@ let build = Metalsmith(__dirname)
   .use(permalinks({
     relative: false
   }))
-  .use(layouts())
+  .use(layouts({
+    engineOptions: {
+      filters: {
+        "hyphen": hyphenFilter
+      }
+    }
+  }))
   .use(htmlMinifier());
 
 if (argv.watch) {
