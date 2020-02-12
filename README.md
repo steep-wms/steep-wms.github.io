5937a9b215f1f1e7f642ec2645bd7e6dc93a3584
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2020-02-12 10:24:30 +0100

    Update README and comments in build file

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# steep-wms.github.io
-Documentation for the Steep Workflow Management System
+# Steep WMS Website
+
+This repository contains the source code of the website of the Steep Workflow
+Management System:
+
+<https://steep-wms.github.io/>
+
+You can find the sources in the `src` directory. The website will be built
+directly into the repository's root directory so GitHub Pages can serve it
+correctly.
+
+If you make changes to the source, rebuild the website and commit both the
+modified sources and the built artefacts together. Push the commit and wait
+for GitHub Pages to update.
+
+Build the website with:
+
+    npm start
+
+Recompile on changes:
+
+    npm run watch
+
+Serve the built website on <http://localhost:5000>:
+
+    npx serve
--- src/build.js
+++ src/build.js
@@ -16,6 +16,7 @@ const sass = require("metalsmith-sass");
 const uglify = require("metalsmith-uglify");
 const watch = require("metalsmith-watch");
 
+// configure hyphenation
 const hyphenateText = hyphenopoly.config({
   "require": ["en-gb"],
   "hyphen": "\u00ad",
@@ -25,76 +26,108 @@ const hyphenateText = hyphenopoly.config({
   "sync": true
 });
 
+// a nunjucks filter that hyphenates text
 function hyphenFilter(str) {
   return new nunjucks.runtime.SafeString(hyphenateText(str));
 }
 
+// a nunjucks extension that automatically inserts SVG code of an icon from the
+// simple-icons package
 function SimpleIconsExtension() {
-    this.tags = ['simpleIcon'];
+  this.tags = ['simpleIcon'];
 
-    this.parse = function(parser, nodes, lexer) {
-      let tok = parser.nextToken();
-      let args = parser.parseSignature(true, true);
-      parser.advanceAfterBlockEnd(tok.value);
-      return new nodes.CallExtension(this, "run", args);
-    };
+  this.parse = function(parser, nodes, lexer) {
+    let tok = parser.nextToken();
+    let args = parser.parseSignature(true, true);
+    parser.advanceAfterBlockEnd(tok.value);
+    return new nodes.CallExtension(this, "run", args);
+  };
 
-    this.run = function(context, name) {
-      return new nunjucks.runtime.SafeString("<i class=\"simple-icon\">" +
-          simpleIcons.get(name).svg + "</i>");
-    };
+  this.run = function(context, name) {
+    return new nunjucks.runtime.SafeString("<i class=\"simple-icon\">" +
+        simpleIcons.get(name).svg + "</i>");
+  };
 }
 
-fs.mkdirSync("js", { recursive: true });
-fs.copyFileSync("node_modules/headroom.js/dist/headroom.min.js", "js/headroom.min.js");
-
+// a markdown renderer that automatically applies hyphenation to text but not
+// headings
 const markdownRenderer = new marked.Renderer();
 markdownRenderer.oldHeadingRenderer = markdownRenderer.heading;
 markdownRenderer.oldTextRenderer = markdownRenderer.text;
 markdownRenderer.text = function(text) {
-    return this.oldTextRenderer(hyphenateText(text));
+  return this.oldTextRenderer(hyphenateText(text));
 };
 markdownRenderer.heading = function(text, level, raw) {
-    return this.oldHeadingRenderer(text.replace(/\u00ad/g, ""), level, raw);
+  return this.oldHeadingRenderer(text.replace(/\u00ad/g, ""), level, raw);
 };
 
+// engine options for the metalsmith-in-place and metalsmith-layouts plugins
 let engineOptions = {
+  // nunjucks filters
   filters: {
     "hyphen": hyphenFilter,
     "date": nunjucksDate
   },
+
+  // nunjucks extensions
   extensions: {
     "simpleIcon": new SimpleIconsExtension()
   },
+
+  // enable smart quotes in markdown
   smartypants: true,
+
+  // enable syntax highlighting in markdown
   highlight: (code, lang) => {
     if (!lang) {
       return code;
     }
     return highlightJs.highlight(lang, code).value
   },
+
+  // set a custom markdown renderer
   renderer: markdownRenderer
 };
 
+// copy required javascript files
+fs.mkdirSync("js", { recursive: true });
+fs.copyFileSync("node_modules/headroom.js/dist/headroom.min.js", "js/headroom.min.js");
+
+// metalsmith build configuration
 let build = Metalsmith(__dirname)
   .source("src")
   .destination("..")
+
+  // do not clean destination directory - it's our repository's root!
   .clean(false)
+
+  // compile SCSS
   .use(sass())
+
+  // uglify JavaScript
   .use(uglify({
     removeOriginal: true
   }))
+
+  // compile markdown files and apply template engine
   .use(inPlace({
     engineOptions
   }))
+
   .use(permalinks({
+    // create a separate directory for each html file
     relative: false
   }))
+
+  // apply layouts
   .use(layouts({
     engineOptions
   }))
+
+  // minify HTML
   .use(htmlMinifier());
 
+// recompile on changes if necessary
 if (argv.watch) {
   build = build.use(
     watch({
@@ -106,6 +139,7 @@ if (argv.watch) {
   );
 }
 
+// start build
 build.build(err => {
   if (err) throw err;
 });
