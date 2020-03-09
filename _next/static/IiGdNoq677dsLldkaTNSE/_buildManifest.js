5deaf6b067a83a7b067904ab61132e4df1d04d9e
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2020-03-09 23:26:51 +0100

    Document data models

--- _next/static/IiGdNoq677dsLldkaTNSE/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/imprint":[a],"/index":[a],"/privacy":[a]}}("static\u002Fchunks\u002F5bdf2d0bced1b77ef92371d90ab29d17d522ab03.6d2ff958df0fb92a71c4.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
