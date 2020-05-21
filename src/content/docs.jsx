5d41346509839377e0203021a91ed7fb678c9a71
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2020-05-21 18:00:41 +0200

    Update Next.js

--- src/content/docs.jsx
+++ src/content/docs.jsx
@@ -1,5 +1,5 @@
-import ScrollLink from "../components/ScrollLink"
-import Sidebar from "../components/Sidebar"
+import ScrollLink from "components/ScrollLink"
+import Sidebar from "components/Sidebar"
 import { cloneElement } from "react"
 
 function spliceToc(currentToc, n) {
