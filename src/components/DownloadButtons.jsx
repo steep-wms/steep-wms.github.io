657a00e5c8e0615f9e81a206557daf006f17eb20
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2020-07-19 13:35:25 +0200

    Bump up version to 5.2.1 and workflow API to 4.1.0

--- src/components/DownloadButtons.jsx
+++ src/components/DownloadButtons.jsx
@@ -1,11 +1,12 @@
 import { Download, GitHub } from "react-feather"
 import { Docker } from "@icons-pack/react-simple-icons"
 import styles from "./DownloadButtons.scss"
+import pkg from "../package.json"
 
 export default () => (
   <div className="download-btns">
-    <a href="https://github.com/steep-wms/steep/releases/download/v5.1.0/steep-5.1.0.zip" className="btn btn-primary">
-      <Download className="feather"/> Download Steep 5.1.0 (binaries)</a>
+    <a href={`https://github.com/steep-wms/steep/releases/download/v${pkg.version}/steep-${pkg.version}.zip`} className="btn btn-primary">
+      <Download className="feather"/> Download Steep {pkg.version} (binaries)</a>
     <a href="https://hub.docker.com/r/steep/steep/" className="btn">
       <Docker className="simple-icon"/> Docker image</a>
     <a href="https://github.com/steep-wms/steep" className="btn">
