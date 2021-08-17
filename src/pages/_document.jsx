dd2ec5b6b93e29cd419d8bd0225dbcb1d0f7bee4
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2021-08-17 16:58:58 +0200

    Set font display to fallback to reduce FOUT

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -12,8 +12,8 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=fallback" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400&display=fallback" rel="stylesheet" />
         </Head>
         <body>
           <Main />
