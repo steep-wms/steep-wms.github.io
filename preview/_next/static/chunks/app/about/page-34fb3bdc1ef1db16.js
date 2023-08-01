cb6024c0c0ccd025f049e515f7b884ab90159bae
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2023-08-01 16:42:52 +0200

    Deploy preview

--- preview/_next/static/chunks/app/about/page-34fb3bdc1ef1db16.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301],{6465:function(n,e,u){Promise.resolve().then(u.bind(u,5712))}},function(n){n.O(0,[68,756,54,712,971,596,744],function(){return n(n.s=6465)}),_N_E=n.O()}]);
