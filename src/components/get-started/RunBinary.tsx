7d4f7f7531ef9a24ecac67aee0a9aef3bc0b5a90
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2023-08-07 07:24:27 +0200

    Revise example workflows

--- src/components/get-started/RunBinary.tsx
+++ src/components/get-started/RunBinary.tsx
@@ -1,17 +1,20 @@
 import pkg from "../../package.json"
+import CodeContainer from "../CodeContainer"
 
 const RunBinary = () => {
   return (
-    <div data-rehype-pretty-code-fragment>
-      <pre>
-        <code>
-          <span style={{ color: "#edf2f7" }}>cd </span>
-          <span style={{ color: "#e16eaa" }}>steep-{pkg.version}</span>
-          {"\n"}
-          <span style={{ color: "#edf2f7" }}>bin/steep</span>
-        </code>
-      </pre>
-    </div>
+    <CodeContainer title="Terminal">
+      <div data-rehype-pretty-code-fragment>
+        <pre data-language="shell" data-theme="default">
+          <code data-language="shell" data-theme="default">
+            <span style={{ color: "#edf2f7" }}>cd </span>
+            <span style={{ color: "#e16eaa" }}>steep-{pkg.version}</span>
+            {"\n"}
+            <span style={{ color: "#edf2f7" }}>bin/steep</span>
+          </code>
+        </pre>
+      </div>
+    </CodeContainer>
   )
 }
 
