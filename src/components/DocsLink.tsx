e63fe2b92217f3fa1ec36ff25f4f085889529ab2
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2023-08-08 16:00:55 +0200

    Auto-link doc sections

--- src/components/DocsLink.tsx
+++ src/components/DocsLink.tsx
@@ -1,7 +1,29 @@
 import Link from "next/link"
 import { AnchorHTMLAttributes } from "react"
+import { Index } from "./docs/Toc"
 
 const DocsLink = (props: AnchorHTMLAttributes<HTMLAnchorElement>) => {
+  let href = props.href!
+  if (href.startsWith("#")) {
+    let entry = Index[href.slice(1)]
+    if (entry === undefined) {
+      throw new Error(`No entry in table of contents for ${href}`)
+    }
+    if (entry.type === "chapter") {
+      throw new Error(`You cannot link to a chapter ${href}`)
+    }
+
+    if (entry.type === "page") {
+      href = `/docs/${entry.slug}`
+    } else {
+      href = `/docs/${entry.page}#${entry.slug}`
+    }
+  }
+
+  if (href !== props.href) {
+    props = { ...props, href }
+  }
+
   return <Link href={props.href!} {...props} />
 }
 
