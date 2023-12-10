bfab3dfc7dda2de914d91ac748d26cee089069a3
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2023-12-10 19:22:18 +0100

    Deploy

---
+++ _next/static/chunks/app/showcase/layout-38a3fd8a2008a2c7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[578],{4471:function(e,n,s){Promise.resolve().then(s.bind(s,8880)),Promise.resolve().then(s.t.bind(s,5250,23)),Promise.resolve().then(s.bind(s,7279))}},function(e){e.O(0,[68,250,791,595,105,971,938,744],function(){return e(e.s=4471)}),_N_E=e.O()}]);
